ce6884b73b17ab13f622810616ebf7b68270a790
paulkreft <paulykreft@gmail.com>
2024-01-08 16:30:47 +0100

    add endpoint to get R&M characters by id and filter by status

--- src/main/java/de/neuefische/paulkreft/externalapiproject/controllers/CharacterController.java
+++ src/main/java/de/neuefische/paulkreft/externalapiproject/controllers/CharacterController.java
@@ -3,11 +3,10 @@ package de.neuefische.paulkreft.externalapiproject.controllers;
 import de.neuefische.paulkreft.externalapiproject.models.Character;
 import de.neuefische.paulkreft.externalapiproject.services.RickAndMortyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/characters")
@@ -16,7 +15,12 @@ public class CharacterController {
     private final RickAndMortyService rickAndMortyService;
 
     @GetMapping
-    public List<Character> getCharacters() {
-         return rickAndMortyService.getCharacters();
+    public List<Character> getCharacters(@RequestParam Optional<String> status) {
+         return rickAndMortyService.getCharacters(status);
+    }
+
+    @GetMapping("/{id}")
+    public Character getCharacterById(@PathVariable int id){
+        return rickAndMortyService.getCharacterById(id);
     }
 }
--- src/main/java/de/neuefische/paulkreft/externalapiproject/services/RickAndMortyService.java
+++ src/main/java/de/neuefische/paulkreft/externalapiproject/services/RickAndMortyService.java
@@ -6,18 +6,29 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RickAndMortyService {
     private final RestClient restClient = RestClient.builder().baseUrl("https://rickandmortyapi.com/api").build();
 
-    public List<Character> getCharacters() {
-        RickAndMortyCharacterResponse response = restClient.get().uri("/character").retrieve().body(RickAndMortyCharacterResponse.class);
+    public List<Character> getCharacters(Optional<String> status) {
+        String uri = "/character";
+
+        if(status.isPresent()) {
+            uri += "?status=" + status.get();
+        }
+
+        RickAndMortyCharacterResponse response = restClient.get().uri(uri).retrieve().body(RickAndMortyCharacterResponse.class);
 
         if(response == null) {
             return List.of();
         }
 
         return response.results();
+    }
+
+    public Character getCharacterById(int id) {
+        return restClient.get().uri("/character/" + id).retrieve().body(Character.class);
     }
 }
