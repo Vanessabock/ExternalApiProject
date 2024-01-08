1af25b3ae0b25fc5ce996877f8ae50c3d183958e
paulkreft <paulykreft@gmail.com>
2024-01-08 15:14:31 +0100

    Initial commit

---
+++ src/main/java/de/neuefische/paulkreft/externalapiproject/ExternalApiProjectApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.paulkreft.externalapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalApiProjectApplication.class, args);
+    }
+
+}
